875043fe8dbf512011328a2d9eed742b0ebd2d64
lpgaming <lpgaming@users.noreply.github.com>
2014-11-04 00:50:05 -0500

    Created the project in Eclipse and shared it with the local clone.

---
+++ pepenet/src/com/lpgo/pepenet/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The purpose of this project is to make a networking library that uses WebSockets and that is as drag-n-drop as possible.
+ */
+/**
+ * @author Edward Bezant. Original author name is unknown, but can be found on GitHub under the online name of "pepedeab".
+ * 
+ * This should be considered a derivative work based on pepedeab's code.
+ */
+package com.lpgo.pepenet;
